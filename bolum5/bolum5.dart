78309871f9732d59f17b93de1cc36eb947ac9a56
github_ata50ata1001 <gserhatakgul@gmail.com>
2022-03-17 22:49:13 +0300

    hafif ilerlemeler

---
+++ bolum5/bolum5.dart
@@ -0,0 +1,73 @@
+/*
+//bolum5 fonksiyonlar ile örnekler
+
+import 'dart:io';
+
+import 'dart:math';
+
+void main(List<String> args) {
+
+dairealan(r = );
+
+
+}
+
+//SORU2
+
+dairealan(double r ,  {double pi = 3.14 }){
+
+print("dairenin alanı = ${pi*r*r} ");
+
+
+
+}
+
+
+/*
+//SORU1
+
+ciftsayilartop(int sayi1){
+
+int? sayi1 = ;
+
+for (var i = 0; i < int1; i++) {
+
+return i ;
+
+
+  
+}
+*/
+
+
+
+  
+
+
+
+
+/*
+hacimbulma(){
+print("hacimi bulmak için iki değer giriniz:");
+
+int alan =alanbulma(en, uzunluk);
+
+int uzunluk = int.parse(stdin.readLineSync()!);
+
+int en = int.parse(stdin.readLineSync()!);
+print("Şimdi yükseklik değerini giriniz: ");
+int yukseklik = int.parse(stdin.readLineSync()!);
+print("bu nesnenin hacmi : ${alan * yukseklik}" );
+
+return;
+}
+
+alanbulma(){
+
+print("Önce alanı bulmak için iki değer girmelisini:");
+
+print("Nesnenin alanı: ${en*uzunluk} ");
+
+return en*uzunluk;
+*/
+*/
